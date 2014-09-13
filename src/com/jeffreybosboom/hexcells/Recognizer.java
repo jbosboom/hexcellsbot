de25c149c3bfce0078778539d6261b64282a40c1
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-09-12 23:46:14 -0400

    Recognizer ditches BufferedImage for lightweight ImageData container

--- src/com/jeffreybosboom/hexcells/Recognizer.java
+++ src/com/jeffreybosboom/hexcells/Recognizer.java
@@ -48,10 +48,48 @@ public final class Recognizer {
 		}
 	}
 
-	private static final int WHITE = Color.WHITE.getRGB();
-	private final ImmutableMap<BufferedImage, Result> references;
+	private static final class ImageData {
+		//grayscale images, so this is one (any one) of the RGB values.
+		private final byte[] data;
+		private final byte width, height;
+		ImageData(byte[] data, byte width, byte height) {
+			this.data = data;
+			this.width = width;
+			this.height = height;
+		}
+		ImageData(BufferedImage image) {
+			this(image, 0, 0, image.getWidth(), image.getHeight());
+		}
+		ImageData(BufferedImage image, int x, int y, int width, int height) {
+			this(new byte[width*height], (byte)width, (byte)height);
+			for (int yp = 0; yp < height; ++yp)
+				for (int xp = 0; xp < width; ++xp) {
+					//TODO: assert grayscale image
+					data[yp * width + xp] = (byte)(image.getRGB(x + xp, y + yp) & 0xFF);
+				}
+		}
+		ImageData(ImageData image, int x, int y, int width, int height) {
+			this(new byte[width*height], (byte)width, (byte)height);
+			for (int yp = 0; yp < height; ++yp)
+				for (int xp = 0; xp < width; ++xp)
+					data[yp * width + xp] = image.at(x + xp, y + yp);
+		}
+		public byte width() {
+			return width;
+		}
+		public byte height() {
+			return height;
+		}
+		public byte at(int x, int y) {
+			return data[y * width() + x];
+		}
+	}
+
+	private static final int WHITE_RGB = Color.WHITE.getRGB();
+	private static final byte WHITE = (byte)(WHITE_RGB & 0xFF);
+	private final ImmutableMap<ImageData, Result> references;
 	public Recognizer() {
-		ImmutableMap.Builder<BufferedImage, Result> builder = ImmutableMap.builder();
+		ImmutableMap.Builder<ImageData, Result> builder = ImmutableMap.builder();
 		for (ConstraintPosition p : ConstraintPosition.values())
 			for (ConstraintKind k : ConstraintKind.values())
 				for (int n = (k == ConstraintKind.NORMAL ? 0 : 2); n < 10; ++n) {
@@ -81,7 +119,7 @@ public final class Recognizer {
 						.filter(e -> e.getValue().number <= 6).iterator()));
 	}
 
-	private static Optional<BufferedImage> cleanCellConstraintImage(Cell.Kind cellKind, BufferedImage subimage) {
+	private static Optional<ImageData> cleanCellConstraintImage(Cell.Kind cellKind, BufferedImage subimage) {
 		Color interiorColor = Colors.HEXAGON_INTERIOR_COLORS.inverse().get(cellKind);
 		Region cellRegion = Region.connectedComponents(subimage, ImmutableSet.of(interiorColor.getRGB()))
 				.stream().sorted(Comparator.comparingInt((Region r_) -> r_.points().size()).reversed()).findFirst().get();
@@ -104,7 +142,7 @@ public final class Recognizer {
 		if (constraintMinX == Integer.MAX_VALUE) return Optional.empty();
 		subimage = subimage.getSubimage(constraintMinX, constraintMinY,
 				constraintMaxX - constraintMinX + 1, constraintMaxY - constraintMinY + 1);
-		return Optional.of(maskOrInvert(copyImage(subimage), interiorColor));
+		return Optional.of(new ImageData(maskOrInvert(copyImage(subimage), interiorColor)));
 	}
 
 	private static BufferedImage maskOrInvert(BufferedImage image, Color maskToWhite) {
@@ -112,7 +150,7 @@ public final class Recognizer {
 			for (int y = 0; y < image.getHeight(); ++y) {
 				int rgb = image.getRGB(x, y);
 				if (rgb == maskToWhite.getRGB())
-					image.setRGB(x, y, WHITE);
+					image.setRGB(x, y, WHITE_RGB);
 				else
 					image.setRGB(x, y, ~rgb | (255 << 24));
 			}
@@ -134,16 +172,17 @@ public final class Recognizer {
 						.filter(e -> e.getValue().number != Result.QUESTION_MARK).iterator()));
 	}
 
-	private static Optional<BufferedImage> cleanBoardEdgeConstraintImage(BufferedImage subimage) {
+	private static Optional<ImageData> cleanBoardEdgeConstraintImage(BufferedImage buffer) {
 		int nonWhiteMinX = Integer.MAX_VALUE, nonWhiteMinY = Integer.MAX_VALUE;
-		for (int x = 0; x < subimage.getWidth(); ++x)
-			for (int y = 0; y < subimage.getHeight(); ++y) {
-				Color rgb = new Color(subimage.getRGB(x, y));
+		for (int x = 0; x < buffer.getWidth(); ++x)
+			for (int y = 0; y < buffer.getHeight(); ++y) {
+				//still need getRGB here to check it's grayscale
+				Color rgb = new Color(buffer.getRGB(x, y));
 				if (rgb.getRed() != rgb.getGreen() ||
 						rgb.getRed() != rgb.getBlue() ||
 						rgb.getGreen() != rgb.getBlue() ||
 						rgb.getRed() >= 220)
-					subimage.setRGB(x, y, WHITE);
+					buffer.setRGB(x, y, WHITE_RGB);
 				else {
 					nonWhiteMinX = Math.min(nonWhiteMinX, x);
 					nonWhiteMinY = Math.min(nonWhiteMinY, y);
@@ -151,38 +190,39 @@ public final class Recognizer {
 			}
 		if (nonWhiteMinX == Integer.MAX_VALUE) return Optional.empty();
 
+		ImageData data = new ImageData(buffer);
 		int nonWhiteMaxX = nonWhiteMinX, nonWhiteMaxY = nonWhiteMinY;
-		outer: while (nonWhiteMaxX < subimage.getWidth()) {
-			for (int y = 0; y < subimage.getHeight(); ++y)
-				if (subimage.getRGB(nonWhiteMaxX, y) != WHITE) {
+		outer: while (nonWhiteMaxX < data.width()) {
+			for (int y = 0; y < data.height(); ++y)
+				if (data.at(nonWhiteMaxX, y) != WHITE) {
 					++nonWhiteMaxX;
 					continue outer;
 				}
 			break;
 		}
-		outer: while (nonWhiteMaxY < subimage.getHeight()) {
-			for (int x = 0; x < subimage.getWidth(); ++x)
-				if (subimage.getRGB(x, nonWhiteMaxY) != WHITE) {
+		outer: while (nonWhiteMaxY < data.height()) {
+			for (int x = 0; x < data.width(); ++x)
+				if (data.at(x, nonWhiteMaxY) != WHITE) {
 					++nonWhiteMaxY;
 					continue outer;
 				}
 			break;
 		}
-		subimage = subimage.getSubimage(nonWhiteMinX, nonWhiteMinY,
+		data = new ImageData(data, nonWhiteMinX, nonWhiteMinY,
 				nonWhiteMaxX - nonWhiteMinX, nonWhiteMaxY - nonWhiteMinY);
 
-		for (int x = 0; x < subimage.getWidth(); ++x)
-			for (int y = 0; y < subimage.getHeight(); ++y)
-				if (new Color(subimage.getRGB(x, y)).getRed() <= 70)
-					return Optional.of(subimage);
+		for (int x = 0; x < data.width(); ++x)
+			for (int y = 0; y < data.height(); ++y)
+				if (Byte.toUnsignedInt(data.at(x, y)) <= 70)
+					return Optional.of(data);
 		return Optional.empty();
 	}
 
-	private static Result compare(BufferedImage needle, Iterator<Map.Entry<BufferedImage, Result>> haystack) {
+	private static Result compare(ImageData needle, Iterator<Map.Entry<ImageData, Result>> haystack) {
 		int bestScore = Integer.MIN_VALUE;
 		Result result = null;
 		while (haystack.hasNext()) {
-			Map.Entry<BufferedImage, Result> e = haystack.next();
+			Map.Entry<ImageData, Result> e = haystack.next();
 			int score = bestFitCompare(needle, e.getKey());
 			if (score > bestScore) {
 				bestScore = score;
@@ -192,32 +232,38 @@ public final class Recognizer {
 		return result;
 	}
 
-	private static int bestFitCompare(BufferedImage a, BufferedImage b) {
-		if (b.getWidth() < a.getWidth() && b.getHeight() < a.getHeight())
-			return bestFitCompare(b, a);
-		else if (!(a.getWidth() <= b.getWidth() && a.getHeight() <= b.getHeight()))
+	private static int bestFitCompare(ImageData needle, ImageData haystack) {
+		if (haystack.width() < needle.width() && haystack.height() < needle.height())
+			return bestFitCompare(haystack, needle);
+		else if (!(needle.width() <= haystack.width() && needle.height() <= haystack.height()))
 			return Integer.MIN_VALUE; //images do not fit inside each other
 
 		int score = 0;
-		if (a.getWidth() == b.getWidth() && a.getHeight() == b.getHeight()) {
-			for (int x = 0; x < a.getWidth(); ++x)
-				for (int y = 0; y < a.getHeight(); ++y)
-					score += (a.getRGB(x, y) != WHITE) == (b.getRGB(x, y) != WHITE)
-							? 1 : -2;
+		if (needle.width() == haystack.width() && needle.height() == haystack.height()) {
+			score = bestFitCompare(needle, haystack, 0, 0);
 		} else {
 			//Try each a-sized subimage of b, penalizing for the size difference.
-			int difference = b.getWidth() * b.getHeight() - a.getWidth() * a.getHeight();
-			for (int x = 0; x < b.getWidth() - a.getWidth(); ++x)
-				for (int y = 0; y < b.getHeight() - a.getHeight(); ++y) {
-					int subscore = bestFitCompare(a, b.getSubimage(x, y, a.getWidth(), a.getHeight()));
+			int difference = haystack.width() * haystack.height() - needle.width() * needle.height();
+			for (int x = 0; x < haystack.width() - needle.width(); ++x)
+				for (int y = 0; y < haystack.height() - needle.height(); ++y) {
+					int subscore = bestFitCompare(needle, haystack, x, y);
 					score = Math.max(score, subscore - difference);
 				}
 		}
 		return score;
 	}
 
+	private static int bestFitCompare(ImageData a, ImageData b, int bx, int by) {
+		int score = 0;
+		for (int x = 0; x < a.width(); ++x)
+			for (int y = 0; y < a.height(); ++y)
+				score += (a.at(x, y) != WHITE) == (b.at(bx + x, by + y) != WHITE)
+						? 1 : -2;
+		return score;
+	}
+
 	private static final Font HARABARA = new Font("Harabara", Font.PLAIN, 16);
-	private static BufferedImage render(String string, int fontSize, ConstraintPosition orientation, boolean antialiased) {
+	private static ImageData render(String string, int fontSize, ConstraintPosition orientation, boolean antialiased) {
 		//It's too hard to compute how big an image to make, so make a super-big
 		//one and crop it later.
 		BufferedImage image = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
@@ -238,24 +284,24 @@ public final class Recognizer {
 		return crop(image);
 	}
 
-	private static BufferedImage crop(BufferedImage image) {
+	private static ImageData crop(BufferedImage image) {
 		int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE,
 				minY = Integer.MAX_VALUE, maxY = Integer.MIN_VALUE;
 		for (int x = 0; x < image.getWidth(); ++x)
 			for (int y = 0; y < image.getHeight(); ++y)
-				if (image.getRGB(x, y) != Color.WHITE.getRGB()) {
+				if (image.getRGB(x, y) != WHITE_RGB) {
 					minX = Math.min(minX, x);
 					maxX = Math.max(maxX, x);
 					minY = Math.min(minY, y);
 					maxY = Math.max(maxY, y);
 				}
-		return image.getSubimage(minX, minY, maxX - minX + 1, maxY - minY + 1);
+		return new ImageData(image, minX, minY, maxX - minX + 1, maxY - minY + 1);
 	}
 
 	public static void main(String[] args) throws Throwable {
 		Recognizer r = new Recognizer();
-		System.out.println(compare(ImageIO.read(
-				new File("constraints/f65281a4a12175cd659ea4e26bbf9361abe0611c.png")),
+		System.out.println(compare(new ImageData(ImageIO.read(
+				new File("constraints/f65281a4a12175cd659ea4e26bbf9361abe0611c.png"))),
 				r.references.entrySet().stream()
 				.filter(e -> e.getValue().pos == ConstraintPosition.TOP)
 				.filter(e -> e.getValue().number <= 6).iterator()));
