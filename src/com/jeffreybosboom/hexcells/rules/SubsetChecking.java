0fe195417355a656d676a15e46ae5eca07f11c6c
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-09-18 11:34:56 -0400

    SubsetChecking supports AxisConstraints

--- src/com/jeffreybosboom/hexcells/rules/SubsetChecking.java
+++ src/com/jeffreybosboom/hexcells/rules/SubsetChecking.java
@@ -8,11 +8,15 @@ import com.jeffreybosboom.hexcells.Constraint;
 import com.jeffreybosboom.hexcells.Coordinate;
 import com.jeffreybosboom.hexcells.Puzzle;
 import java.util.ArrayDeque;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashSet;
+import java.util.IntSummaryStatistics;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 /**
@@ -34,7 +38,7 @@ public final class SubsetChecking implements Function<Puzzle, Puzzle> {
 			Set<Coordinate> alwaysPresent = new HashSet<>(unknown), alwaysAbsent = new HashSet<>(unknown);
 			for (Set<Coordinate> s : Sets.powerSet(unknown)) {
 				if (s.size() != deficit) continue;
-				if (isContiguous(cons, Sets.union(present, s)) != cons.isContiguous()) continue;
+				if (isContiguous(t, cons, Sets.union(present, s)) != cons.isContiguous()) continue;
 				alwaysAbsent.removeAll(s);
 				alwaysPresent.removeAll(Sets.difference(unknown, s));
 			}
@@ -47,7 +51,7 @@ public final class SubsetChecking implements Function<Puzzle, Puzzle> {
 		return t;
 	}
 
-	private boolean isContiguous(Constraint cons, Set<Coordinate> present) {
+	private static boolean isContiguous(Puzzle p, Constraint cons, Set<Coordinate> present) {
 		if (cons instanceof CellConstraint) {
 			//set is contiguous if we can get anywhere from anywhere, so flood fill
 			//from an arbitrary point
@@ -61,7 +65,19 @@ public final class SubsetChecking implements Function<Puzzle, Puzzle> {
 			}
 			return closed.equals(present);
 		} else if (cons instanceof AxisConstraint) {
-			throw new UnsupportedOperationException("TODO");
+			ToIntFunction<Coordinate> axisExtractor = ((AxisConstraint)cons).axisExtractor();
+			ToIntFunction<Coordinate> sortExtractor = ((AxisConstraint)cons).sortAxisExtractor();
+			int axisValue = axisExtractor.applyAsInt(cons.region().get(0));
+			List<Coordinate> axis = p.cells()
+					.filter(c -> axisExtractor.applyAsInt(c) == axisValue)
+					.sorted(Comparator.comparingInt(sortExtractor))
+					.collect(Collectors.toList());
+			IntSummaryStatistics stats = axis.stream()
+					.filter(c -> p.isPresent(c) || present.contains(c))
+					.mapToInt(axis::indexOf)
+					.summaryStatistics();
+			assert stats.getMin() != -1;
+			return (stats.getMax() - stats.getMin() + 1) == cons.target();
 		} else
 			throw new UnsupportedOperationException(cons.getClass().getSimpleName()+" "+cons);
 	}
